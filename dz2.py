14c076c431e144531182cfeea8578fbca0afb99a
jenycher <168509289+jenycher@users.noreply.github.com>
2024-06-25 06:22:27 +0300

    Update dz2.py

--- dz2.py
+++ dz2.py
@@ -7,12 +7,15 @@ import requests
 import pprint
 
 params = {
-    'userId' : '1'
+    'userId' : 1
 }
 
 response = requests.get('https://jsonplaceholder.typicode.com/posts', params=params)
 
-response_json = response.json()
+
 print(f"Статус-код= {response.status_code}")
-pprint.pprint(response_json)
+posts = response.json()
+for post in posts:
+    print(post)
+
 
